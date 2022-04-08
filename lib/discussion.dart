56c4920c4ca3ecee0c67858a2a2ec85d5f67c871
GACQUADRO <73235291+GACQUADRO@users.noreply.github.com>
2022-04-08 12:17:12 +0200

    pas bon

--- lib/discussion.dart
+++ lib/discussion.dart
@@ -18,19 +18,24 @@ class discussion extends StatefulWidget {
 //${firebaseFonc().cur_user?.email} => l'utilisateur qui est connecté
 //${widget.user.pseudo} => l'utilisateur avec qui on veut parler
 
-
 class discussionState extends State<discussion> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
-          title: Text("${widget.user.pseudo}"),
-        ),
-        body: Center(
-          child: bodyPage(),
-        ));
-  }
+      appBar: AppBar(
+        title: Text("${widget.user.pseudo}"),
+      ),
+      body: Center(
+        child: bodyPage(),
+      ),
+      /*appBar: AppBar(
+        title: inputZone(),
+      ),*/
 
+      //bottomSheet: inputZone(),
+      //bottomNavigationBar: inputZone()
+    );
+  }
 
   final myController = TextEditingController();
 
@@ -48,8 +53,10 @@ class discussionState extends State<discussion> {
                   Messages msg = Messages(documents[index]);
                   print(firebaseFonc().cur_user?.email);
 
-                  if(msg.sendBy == firebaseFonc().cur_user?.email && msg.sendFor == widget.user.mail ){
-
+                  if ((msg.sendBy == firebaseFonc().cur_user?.email &&
+                          msg.sendFor == widget.user.mail) ||
+                      (msg.sendBy == widget.user.mail &&
+                          msg.sendFor == firebaseFonc().cur_user?.email)) {
                     return Card(
                       elevation: 5.0,
                       shape: RoundedRectangleBorder(
@@ -63,20 +70,60 @@ class discussionState extends State<discussion> {
                         ));*/
                         },
                         title: Text("${msg.content}"),
-
                       ),
                     );
+                  } else {
+                    //Le code veut un else donc j'ai mis ca, ca permet d'avoir rien d'afficher mais ca met un petit espace,
+                    // si tu trouves mieux c'est cool
 
-                  } else { //Le code veut un else donc j'ai mis ca, ca permet d'avoir rien d'afficher mais ca met un petit espace,
-                          // si tu trouves mieux c'est cool
                     return Text("");
-
-
-
                   }
-
                 });
           }
         });
+  }
+
+  Widget inputZone() {
+    return Form(
+      child: Column(children: <Widget>[
+        SizedBox(height: 50),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: myController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Send a message',
+                  ),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              backgroundColor: Colors.blue,
+              onPressed: () {
+                builder:
+                (context) {
+                  return AlertDialog(
+                    content: Text(myController.text),
+                  );
+                };
+                DateTime times = DateTime.now();
+                firebaseFonc().sendMsg(myController.text,
+                    firebaseFonc().cur_user?.email, widget.user.mail, times);
+              },
+              child: Icon(
+                Icons.arrow_right_rounded,
+                size: 45,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
   }
 }
